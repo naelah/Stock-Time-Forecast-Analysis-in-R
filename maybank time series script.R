1a5f19f46dc80b8b994a40c7f934a6cf8fe47d5b
naelah <naelahnordin@gmail.com>
2019-12-09 23:37:42 +0800

    added R script

---
+++ maybank time series script.R
@@ -0,0 +1,31 @@
+#install.packages('TTR')
+#install.packages('matrixStats')
+#install.packages('StanHeaders')
+#install.packages('extraDistr')
+#install.packages('rstan')
+#install.packages('prophet')
+#install.packages('Rtools')
+
+maybank.raw.data = read.csv('Maybank for R.csv')
+str(maybank.raw.data)
+
+#Extract data for prophet
+
+ds = as.Date(maybank.raw.data$DATE, format="%d/%m/%Y")
+y = maybank.raw.data$DAILY.RETURN
+maybank.data = data.frame(ds,y)
+str(maybank.data)
+
+library(prophet)
+m <- prophet(maybank.data)
+future <- make_future_dataframe(m, periods = 365)
+tail(future)
+
+forecast <- predict(m, future)
+tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
+
+plot(m, forecast)
+
+prophet_plot_components(m, forecast)
+
+dyplot.prophet(m, forecast)
